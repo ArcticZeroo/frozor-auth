4acae6262ba750381dedca05854a9545600d867c
Spencer <spencer@frozor.io>
2023-02-22 21:01:02 -0800

    Add app container with background color

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import { MainTabView } from './components/pages/tab-view';
 import { EmailContext } from './context/auth';
 import { PageStateContext } from './context/page';
 import { PageState } from './models/page';
+
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  padding: 1rem;
+`;
 
 function App() {
 	const [pageState, setPageState] = useState(PageState.login);
@@ -11,7 +19,9 @@ function App() {
 	return (
 		<PageStateContext.Provider value={{ pageState, setPageState }}>
 			<EmailContext.Provider value={{ email, onEmailChanged: setEmail }}>
-				<MainTabView/>
+				<AppContainer>
+					<MainTabView/>
+				</AppContainer>
 			</EmailContext.Provider>
 		</PageStateContext.Provider>
 	);
